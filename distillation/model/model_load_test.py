619efad915376aeca453585bcf66099fc8b0cc1a
ppijbb <ppijbb@naver.com>
2025-05-13 14:50:26 +0900

    refactor: Rename and update G3MoE model classes for consistency and clarity
    - Renamed classes and variables from Gemma3 to G3MoE for improved consistency across the codebase.
    - Updated model_load_test.py to reflect the new G3MoEForCausalLM class and adjusted configuration parameters for expert routing.
    - Enhanced comments and documentation to clarify the purpose and functionality of the updated classes.

--- distillation/model/model_load_test.py
+++ distillation/model/model_load_test.py
@@ -2,7 +2,7 @@ import torch
 from g2moe_config import G2MoEConfig
 from g2moe_model import G2MoEForCausalLM
 from g3moe_config import G3MoEConfig
-from g3moe_model import Gemma3ForCausalLM
+from g3moe_model import G3MoEForCausalLM
 from transformers import AutoTokenizer, GenerationConfig
 import tensorrt
 print("version of tensorrt: " ,tensorrt.__version__)
@@ -15,13 +15,13 @@ def format_parameters(number):
     else:
         return str(number)
 
-model_architecture = Gemma3ForCausalLM
+model_architecture = G3MoEForCausalLM
 
 test_model = model_architecture.from_pretrained(
     pretrained_model_name_or_path="google/gemma-3-1b-it",
     config=G3MoEConfig(
         n_shared_experts=1,
-        n_routed_experts=6, # 15, 6, 1
+        n_routed_experts=3, # 15, 6, 1
         n_group=2,
         topk_group=2,
         num_experts_per_tok=1,
